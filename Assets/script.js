e7914453df60546fb9aa0ed44a7ce98bade9ea66
hynookim98 <hynookim98@gmail.com>
2022-11-19 14:31:35 -0800

    started on javascript..

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,25 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(function () { 
+
+  $('.saveBtn').on("click", grabData);
+
+  function grabData() {
+
+    localStorage.setItem($(this).siblings('.description').val(), $(this).parent().attr('id'));
+
+  }
+
+  function refreshHour() {
+    var hour = daysjs().hour();
+
+    $('time-block').each(function() {
+      var appHour = 
+    })
+  }
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
