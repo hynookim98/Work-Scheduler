bf896a4b7d7485c358c4814f97871cb3b18c54a5
hynookim98 <hynookim98@gmail.com>
2022-11-20 20:39:30 -0800

    added code to save for all hours

--- Assets/script.js
+++ Assets/script.js
@@ -61,6 +61,14 @@ $(function () {
 
 
   // code to grab each specific code box and manually change text area content value to stored data
+  $('#hour-1 .description').val(localStorage.getItem('hour-1'));
+  $('#hour-2 .description').val(localStorage.getItem('hour-2'));
+  $('#hour-3 .description').val(localStorage.getItem('hour-3'));
+  $('#hour-4 .description').val(localStorage.getItem('hour-4'));
+  $('#hour-5 .description').val(localStorage.getItem('hour-5'));
+  $('#hour-6 .description').val(localStorage.getItem('hour-6'));
+  $('#hour-7 .description').val(localStorage.getItem('hour-7'));
+  $('#hour-8 .description').val(localStorage.getItem('hour-8'));
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
@@ -70,6 +78,13 @@ $(function () {
   $('#hour-15 .description').val(localStorage.getItem('hour-15'));
   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
   $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  $('#hour-18 .description').val(localStorage.getItem('hour-18'));
+  $('#hour-19 .description').val(localStorage.getItem('hour-19'));
+  $('#hour-20 .description').val(localStorage.getItem('hour-20'));
+  $('#hour-21 .description').val(localStorage.getItem('hour-21'));
+  $('#hour-22 .description').val(localStorage.getItem('hour-22'));
+  $('#hour-23 .description').val(localStorage.getItem('hour-23'));
+  $('#hour-24 .description').val(localStorage.getItem('hour-24'));
 
   $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
 
